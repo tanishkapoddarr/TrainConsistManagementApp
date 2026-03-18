39573da587f6f968540294e99e5d8e4cdd8d24b5
tanishkapoddarr <tp1002@srmist.edu.in>
2026-03-18 10:38:02 +0530

    UC5:import java.util.*;

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,24 +1,20 @@
 import java.util.*;
-public class TrainConsistManagementApp {
+public class TrainConsistManagementApp  {
     public static void main(String[] args) {
         System.out.println("==============================================");
-        System.out.println(" UC4 - Maintain Ordered Bogie Consist ");
+        System.out.println(" UC5 - Preserve Insertion Order of Bogies ");
         System.out.println("==============================================\n");
-        List<String> trainConsist = new LinkedList<>();
-        trainConsist.add("Engine");
-        trainConsist.add("Sleeper");
-        trainConsist.add("AC");
-        trainConsist.add("Cargo");
-        trainConsist.add("Guard");
-        System.out.println("Initial Train Consist:");
-        System.out.println(trainConsist + "\n");
-        trainConsist.add(2, "Pantry Car");
-        System.out.println("After Inserting 'Pantry Car' at position 2:");
-        System.out.println(trainConsist + "\n");
-        trainConsist.remove(0);
-        trainConsist.remove(trainConsist.size() - 1);
-        System.out.println("After Removing First and Last Bogie:");
-        System.out.println(trainConsist + "\n");
-        System.out.println("UC4 ordered consist operations completed...");
+        Set<String> formation = new LinkedHashSet<>();
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        formation.add("Sleeper");
+        formation.add("Engine");
+        System.out.println("Final Train Formation:");
+        System.out.println(formation + "\n");
+        System.out.println("Note:");
+        System.out.println("LinkedHashSet preserves insertion order and removes duplicates automatically.\n");
+        System.out.println("UC5 formation setup completed...");
     }
 }
